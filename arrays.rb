98f0eae0a9f4bb0722dc211ece0572fffc316dc7
Val Cooper <vcooper@DTLWATLVALCO.local>
2019-03-14 13:13:14 -0400

    hashes

--- arrays.rb
+++ arrays.rb
@@ -26,3 +26,61 @@ p a.last
  # or to save the mutation
  x.to_a.shuffle!
 
+# reverse the array and mutate
+x.reverse!
+
+# add item to end of array shovel operator
+
+x << 10
+# or
+x.append(10)
+
+# add item to beginning of array
+
+x.unshift(0)
+
+# remove duplicates from array and mutate
+
+x.uniq!
+
+# check if a value exist
+
+x.include?(10)
+
+# join items in array and turn into string
+
+x.join(", ")
+x.join("-")
+
+# turn string into an array
+
+y = x.join("-")
+y.split("-")
+# this will look for the dash in the string and seperate based on any parameter given and turn into array;
+
+# make an array
+%w(my name is valencia)
+# => ["my", "name", "is", "valencia"]
+
+# assigned values to a varibale using the last expression
+
+z= _
+# this will grab the array above and store it into the varibale z
+
+# loop through array
+for i in z
+    print i
+end
+
+# or 
+
+z.each do |food|
+    print food + ""
+end
+ # or
+
+ z.each {|food| print food + ""}
+
+ # using the select method test for bools
+ z=(1..100).to_a.shuffle
+ z.select{|number| number.odd?}
---
+++ hash.rb
@@ -0,0 +1,34 @@
+# define a hash
+my_details = {'name' => 'valencia', 'age' => 29, 'favcolor' => 'black'}
+
+# access values within the hash
+p my_details['name']
+
+# or 
+another_hash = {name: "valencia", age: 29, favcolor: 'black'}
+p another_hash[:name]
+
+# find all keys
+
+another_hash.keys
+# find all values
+another_hash.values
+
+# loop through hash
+
+another_hash.each do |key,value|
+puts "the class for key is #{key.class} and the value is #{value}"
+end
+
+# add values to the hash
+
+myHash= {a: 1, b: 2, c:3}
+myHash[:d] = 4
+
+# change existing values
+myHash[:a] = "Ruby"
+
+myHash.each {|some_key, some_value| puts "they key is #{some_key} and the vale is #{some_value}"}
+
+# find items using the select method
+myHash.select{|key, value| value.is_a?(String)}
